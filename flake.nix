e21e2f0e16718c232ee3f2308c2d74230ffcceae
Michael Hueschen <m@mhueschen.dev>
2024-01-25 09:15:55 -0700

    doc + make it work

--- flake.nix
+++ flake.nix
@@ -13,6 +13,15 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        devPkgs = with pkgs; [
+          cmake
+          glfw
+          glib
+          libGL
+          mesa
+          xorg.libX11
+        ];
       in
       {
         devShell =
@@ -20,11 +29,12 @@
             buildInputs = with pkgs; [
               (python310.withPackages (ps: with ps; [
               ]))
-            ];
+            ] ++ devPkgs;
             shellHook = ''
               addToSearchPath "LD_LIBRARY_PATH" "${pkgs.lib.getLib pkgs.stdenv.cc.cc}/lib"
               addToSearchPath "LD_LIBRARY_PATH" "/run/opengl-driver/lib"
             '';
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devPkgs;
           };
       });
 
